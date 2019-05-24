c64352f8b73362ef7330bfff031f53438c3e6512
USER <USER@AMP-27>
2019-05-24 09:59:17 +0900

    sfsfsffsdf

---
+++ chapter12_1/src/chapter12_1/paintJPanelEx.java
@@ -0,0 +1,35 @@
+package chapter12_1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
+public class paintJPanelEx extends JFrame {
+	private MyPanel panel = new MyPanel();
+	
+	public paintJPanelEx(){
+		setTitle("Jpanel¿« component() øπ¡¶");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(250,220);
+		setVisible(true);
+}
+	class MyPanel extends JPanel{
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			g.setColor(Color.BLUE);
+			g.drawRect(10,10,50,50);
+			g.drawRect(50,50,50,50);
+			g.setColor(Color.MAGENTA);
+			g.drawRect(90, 90, 50, 50);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new paintJPanelEx();
+	}
+}
